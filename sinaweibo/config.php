a0bdbe8539dbbd5d0122ca02ccc757dda520d06a
ngxiaoyi <ngxiaoyi@gmail.com>
2013-06-24 22:52:19 +0800

    delete config
    delete config.php

--- sinaweibo/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-	网站接入的配置信息，需要在joomla后台模块配置选项中供用户输入参数，此处动态调用参数信息。
-*/
-header('Content-Type: text/html; charset=UTF-8');
-
-define( "WB_AKEY" , '1281929385' );
-define( "WB_SKEY" , 'a318dc91d5f9c6ad1ccd079774a57af3' );
-define( "WB_CALLBACK_URL" , 'http://www.ruanjian081.com/administrator/modules/mod_sinaweibo/sinaweibo/callback.php' );
